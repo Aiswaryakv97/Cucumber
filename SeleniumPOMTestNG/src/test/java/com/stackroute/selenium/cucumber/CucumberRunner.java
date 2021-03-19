f9380bcf9597f0c9354649c49d5734635492ccf5
Aiswaryakv97 <aiswaryakviyer@gmail.com>
2021-03-19 15:16:43 +0530

    added

---
+++ SeleniumPOMTestNG/src/test/java/com/stackroute/selenium/cucumber/CucumberRunner.java
@@ -0,0 +1,11 @@
+package com.stackroute.selenium.cucumber;
+
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features="/home/ubuntu/Downloads/seleniumalldemos-wave27-master-SeleniumPOMTestNG/SeleniumPOMTestNG/src/test/java/com/stackroute/selenium/cucumber",
+
+glue="com.stackroute.selenium.cucumber")
+public class CucumberRunner {
+
+}
+
